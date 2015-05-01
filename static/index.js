4800f20b757fc67ad8a9e2a3734fc0e99fcb8a4b
StevenZabolotny <stevezabolotny@gmail.com>
2015-05-01 13:18:23 -0400

    Added pretty textbox animations.

--- static/index.js
+++ static/index.js
@@ -2,15 +2,19 @@ console.log("HELLO");
 
 var comp = d3.select("#comp");
 var a = d3.select("#a")
+var typebool = false;
 
 var fb = d3.select("#chat")
     .on("click", function() {
-	a.select("textarea")
-	    .remove();
-	a.select("br")
-	    .remove();
-	a.select("br")
-	    .remove();
+	if (typebool == true) {
+	    a.select("textarea")
+		.transition()
+		.duration(500)
+		.style("width", "0px")
+		.style("height", "0px")
+		.remove();
+	    typebool = false;
+	}
 	/*FB.api(
 	    "/me",
 	    function (response) {
@@ -20,25 +24,39 @@ var fb = d3.select("#chat")
 	);*/
     });
 var type = d3.select("#type")
-	.on("click", function() {
+    .on("click", function() {
+	if (typebool == false) {
 	    a.append("textarea")
-		.attr("name", "a")
-		.attr("rows", 4)
-		.attr("cols", 50)
+	    	.attr("name", "a")
+		.style("width", "0px")
+		.style("height", "0px") 
+		.style("margin-bottom", "20px")
+		.transition()
+		.duration(750)
+		.style("width", "559px")
+		.style("height", "107px")
+		.transition()
+		.delay(750)
 		.text("Anonymous Text Goes Here!");
-	    a.append("br");
-	    a.append("br");
-	});
+	    typebool = true;
+	}
+    });
 	
 var count = 1; // Keeps count of how many textareas you have added
 var button = d3.select("#add")
     .on("click", function() {
 	count++;
-	comp.append("br");
-	comp.append("br");
+	comp.append("br")
 	comp.append("textarea")
 	    .attr("name", count)
-	    .attr("rows", 4)
-	    .attr("cols", 50)
+	    .style("width", "0px")
+	    .style("height", "0px") 
+	    .style("margin-bottom", "20px")
+	    .transition()
+	    .duration(750)
+	    .style("width", "559px")
+	    .style("height", "107px")
+	    .transition()
+	    .delay(750)
 	    .text("Comparison Text #" + count + " Goes Here!");
     });

5e66a76e79074f143778c2de221f60f656e8157a
Richard <zhan97richard@gmail.com>
2015-05-04 20:25:04 -0400

    Oops, forgot to push this

--- parse.py
+++ parse.py
@@ -1,16 +1,20 @@
-#why is python such a shitty language?
+#why is python such a good language?
 
-import nltk
+#import nltk
 import re
 from collections import Counter
 import operator
 
-sentence = "A cat ate a dog in the                     forest!!!!!! >.< >////< XD so rundum."
+sentence = "A cat ate a dog in the           \n          forest!!!!!! >.< >////< XD so rundum.\n#LOL #SORRYNOTSORRY #TRUF"
+
+
 emoticon_regex = re.compile('')
 emoticon_unicode_regex = re.compile('')
 
+
+
 PUNCTUATIONS = "!:\-,;.!?~"
-punctuation_regex = re.compile("(?:([\w]+)([" + PUNCTUATIONS + "]+)\s)")
+punctuation_regex = re.compile("(?:([\w]+)([" + PUNCTUATIONS + "]+)\s?)")
 
 def is_emoticon(s):
     return 0
@@ -23,8 +27,8 @@ def split_punctuations(tokens):
     for token in tokens:
         match = re.match(punctuation_regex, token)
         if match:
-            new_tokens.append(match.group(0))
             new_tokens.append(match.group(1))
+            new_tokens.append(match.group(2))
         else:
             new_tokens.append(token)
     return new_tokens
@@ -52,5 +56,9 @@ class ChatParser:
 d = ChatParser(sentence)
 
 print d.parseParser()
+
+d.addFragment("gdi, you are so bhed\n Plz")
+
+d.parseParser()
 
 print d.tokens

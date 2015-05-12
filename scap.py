a313c5ca735a2f75288dc3a84149fccdac929d0a
Richard <zhan97richard@gmail.com>
2015-05-12 13:23:54 -0400

    ziwei did tid this

--- scap.py
+++ scap.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 # This is the Source Code Authorship Profile (SCAP) method
 # Based primarily on "Identifying Authorship by Byte-Level N-Grams:The Source Code Author Profile (SCAP) Method"
 # Publicly available at https://www.utica.edu/academic/institutes/ecii/publications/articles/B41158D1-C829-0387-009D214D2170C321.pdf
@@ -10,27 +12,24 @@ N_GRAM_MAX = 6
 def find_ngram(input_list, n):
     assert n >= 2
     return zip(*[input_list[i:] for i in range(n)])
-     
-def hexchar_to_dec(c):
-    return int(c.encode('hex'), 16)
 
-def utf_to_octet(s):
-    return map(hexchar_to_dec, s)
-
-def find_octet_ngram(octet):
-    return find_ngram(octet, 2) + find_ngram(octet, 3) + find_ngram(octet, 4) + find_ngram(octet, 6) + find_ngram(octet, 8) + find_ngram(octet, 16) + find_ngram(octet, 32)
+def find_uni_ngram(s):
+    x = []
+    for k in range(N_GRAM_MIN, N_GRAM_MAX+1):
+        x.append(find_ngram(s, y))
+    return x
 
 # Class for author profile
 class Profile:
     def __init__(self, name="", *rest_text):
         self.name = name
-        if rest_test:
+        self.texts = []
+        if rest_text:
             apply(self.add_texts, rest_text)
 
     # Add a single text
     def add_text(self, new_text):
-        text_profile = self.analyze_text(new_text)
-        self.profile = self.merge_profiles(self.profile, text_profile)
+        self.texts.append(new_text)
 
     # For every n-gram between N_GRAM_MIN and N_GRAM_MAX:
         # ... Count n-grams
@@ -38,18 +37,18 @@ class Profile:
         # ... Discard frequency to save memory (?)
     # Return array of arrays
     def analyze_text(self, new_text):
-        pass
+        self.ngrams = find_uni_ngrams(self.texts)
+        hash_table = {}
+        for x in 
 
     # Add multiple texts
     def add_texts(self, *new_texts):
         for new_text in new_texts:
             self.add_text(new_text)
 
-    def merge_profiles(self, profile1, profile2):
-        pass
-
     # For each n-gram, use the shorter profile for comparison
     # Similarity should probably be an _absolute_ measure of intersection
     # Not a proportion
-    def find_distance(self, other_profile):
-        pass
+    
+def find_distance(p1, p2):
+    pass

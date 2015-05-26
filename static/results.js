422e5a458c53f78c151909148cc65fd4d8c19158
Philipp Steinmann <steinmann.philipp@yahoo.com>
2015-05-25 22:56:32 -0400

    Improve the JS

--- static/results.js
+++ static/results.js
@@ -1,28 +1,49 @@
-console.log("HELLO");
-
 var content = d3.select("#content");
-var a = content.selectAll("h3");
-var t;
-var p = 0;
-var max = "";
+var results = content.selectAll("h3");
+var result;
+var similarity = 0;
+var max = [];
 var pmax = 0;
 var percents = {};
 
-for (var i = 0; i < a[0].length;i++) {
-    t = a[0][i];
-    p = parseInt(t.innerHTML[t.innerHTML.search(": ") + 2] + t.innerHTML[t.innerHTML.search(": ") + 3]);
-    percents[t.id] = p;
-    if (p >= pmax) {
-	pmax = p;
-	max = t.id;
+var confidence_threshold = 0.1;
+
+for (var i = 0; i < results[0].length;i++) {
+    result = results[0][i];
+    similarity = parseInt(result.innerHTML[result.innerHTML.search(": ") + 2] + result.innerHTML[result.innerHTML.search(": ") + 3]);
+
+    percents[result.id] = similarity;
+    if (similarity == pmax) {
+        max.push(result.id);
     }
 
-    d3.select("#" + t.id)
-	.style("color", "rgb(" + (255 - (255 * (p / 100))) + ", " + (255 * (p / 100)) + ", 0");
+    if (similarity > pmax) {
+        pmax = similarity;
+        max = [result.id];
+    }
 
-    if (i == a[0].length - 1) {
+    d3.select("#" + result.id)
+    .style("color", "rgb(" + (255 - (255 * (similarity / 100))) + ", " + (255 * (similarity / 100)) + ", 0");
+
+    if (i == results[0].length - 1) {
+        var message;
+        if (pmax == 0)
+            message = "None of the candidates had any similarity to the anonymous text."
+
+        if (pmax < confidence_threshold)
+            message = "None of the candidates had sufficient similarity to the anonymous text.";
+        
+        if (pmax >= confidence_threshold) {
+            if (max.length == 1)
+                message = "The closest match was " + max[0] + " with a similarity percentage of " + pmax + "%"
+
+            else {
+                var result_names = max.join(" and ");
+                message = "The results were tied at " + pmax + "% between " + results_names;
+            }
+        }
 	content.insert("h2")
-	    .text("The closest match was " + max + " with a similarity percentage of " + pmax + "%!");
+	    .text(message);
     }
-}
 
+}

8080081a7f3b6e57d2a3fac0eaaa7abee4cb15b9
StevenZabolotny <stevezabolotny@gmail.com>
2015-05-28 13:44:54 -0400

    Fixed similarity percentage and results coloration.

--- static/results.js
+++ static/results.js
@@ -14,14 +14,14 @@ for (var i = 0; i < results[0].length;i++) {
     for (var j = result.innerHTML.search(": ") + 3;j < result.innerHTML.length - 2;j++) {
 	slist = slist + result.innerHTML[j];
     }
-    console.log(result.innerHTML[result.innerHTML.search(": ") + 3]);
-    console.log(slist);
     slist = slist.split(", ");
+    console.log(slist);
     for (var j = 0;j < slist.length;j++) {
-	slist[j] = parseInt(slist[j]);
+    	slist[j] = parseFloat(slist[j]);
     }
+    //    slist = map(slist,function(j){ parseFloat(j); }); map is not included :(
     //similarity = parseInt(result.innerHTML[result.innerHTML.search(": ") + 2] + result.innerHTML[result.innerHTML.search(": ") + 3]);
-    similarity = slist[0] * 2 + slist[1] * 3 + slist[2] * 4 + slist[3] * 5 + slist[4] * 6;
+    similarity = (slist[0] * 2 + slist[1] * 3 + slist[2] * 4 + slist[3] * 5 + slist[4] * 6) / (2 + 3 + 4 + 5 + 6);
     
     percents[result.id] = similarity;
     if (similarity == pmax) {
@@ -29,12 +29,13 @@ for (var i = 0; i < results[0].length;i++) {
     }
 
     if (similarity > pmax) {
-        pmax = similarity;
+        pmax = parseInt(similarity * 100);
         max = [result.id];
     }
 
     d3.select("#" + result.id)
-	.style("color", "rgb(" + (255 - (255 * (similarity / 100))) + ", " + (255 * (similarity / 100)) + ", 0");
+    //.style("color", "rgb(" + parseInt(255 - (255 * similarity)) + ", " + parseInt(255 * similarity) + ", 0)");
+	.style("color", "rgb(0, " + (100 + parseInt(155 * similarity)) + ", 0)");
 
     if (i == results[0].length - 1) {
         var message;
@@ -50,7 +51,7 @@ for (var i = 0; i < results[0].length;i++) {
 
             else {
                 var result_names = max.join(" and ");
-                message = "The results were tied at " + pmax + "% between " + results_names + ".";
+                message = "The results were tied at " + pmax + "% between " + result_names + ".";
             }
         }
 	content.insert("h2")
